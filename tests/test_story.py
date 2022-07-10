1050e87bf5e6f0ffd4a2ee09bd7a95caaa5e27cb
David Vegh <david.andras.vegh+github@gmail.com>
2022-07-10 10:35:11 +0200

    tests: fix format

--- tests/test_story.py
+++ tests/test_story.py
@@ -49,7 +49,9 @@ class TestJupyterStory(TestHtml, unittest.TestCase):
             "ipyvizzustory.storylib.story.uuid.uuid4", return_value=self
         ):
             with unittest.mock.patch("ipyvizzustory.jupyter.story.get_html") as output:
-                with unittest.mock.patch("ipyvizzustory.jupyter.story.display_html_code"):
+                with unittest.mock.patch(
+                    "ipyvizzustory.jupyter.story.display_html_code"
+                ):
                     self.get_story().play()
                     self.assertEqual(
                         output.call_args_list[0].args[0],
