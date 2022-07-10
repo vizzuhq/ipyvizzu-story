14e50f19ee2236b1b03d37227db8d5ad6b9b36ae
David Vegh <david.andras.vegh+github@gmail.com>
2022-07-10 10:41:39 +0200

    tests: try with autospec

--- src/ipyvizzustory/jupyter/story.py
+++ src/ipyvizzustory/jupyter/story.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 
-from IPython.display import display as display_html_code, HTML as get_html
+from IPython.display import display, HTML
 
 from ipyvizzu import Data, Style
 
@@ -21,5 +21,5 @@ class Story(StoryLib):
     def play(self) -> None:
         """A method for displaying the html code."""
 
-        html = get_html(self.to_html())
-        display_html_code(html)
+        html = HTML(self.to_html())
+        display(html)
--- tests/test_story.py
+++ tests/test_story.py
@@ -45,13 +45,15 @@ class TestJupyterStory(TestHtml, unittest.TestCase):
     def test_play(self) -> None:
         """A method for testing Story().play()."""
 
-        with unittest.mock.patch(
-            "ipyvizzustory.storylib.story.uuid.uuid4", return_value=self
-        ):
-            with unittest.mock.patch("ipyvizzustory.jupyter.story.get_html") as output:
+        with unittest.mock.patch("ipyvizzustory.jupyter.story.display", autospec=True):
+            with unittest.mock.patch(
+                "ipyvizzustory.storylib.story.uuid.uuid4",
+                autospec=True,
+                return_value=self,
+            ):
                 with unittest.mock.patch(
-                    "ipyvizzustory.jupyter.story.display_html_code"
-                ):
+                    "ipyvizzustory.jupyter.story.HTML", autospec=True
+                ) as output:
                     self.get_story().play()
                     self.assertEqual(
                         output.call_args_list[0].args[0],
