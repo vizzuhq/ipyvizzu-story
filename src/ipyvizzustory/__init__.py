dcfac2e56ce7e95723f3ac502499891c866339ea
David Vegh <61405792+veghdev@users.noreply.github.com>
2022-07-15 10:48:24 +0200

    ipyvizzustory: fix env import

--- src/ipyvizzustory/__init__.py
+++ src/ipyvizzustory/__init__.py
@@ -6,12 +6,20 @@ from .storylib.story import Step, Slide
 from .env.py.story import Story as PythonStory
 
 try:
+    import IPython  # type: ignore
     from .env.ipy.story import Story as JupyterStory
-except ImportError:  # pragma: no cover
+
+    if not IPython.get_ipython():  # pragma: no cover
+        raise ImportError("JupyterStory")
+except ImportError as e:  # pragma: no cover
     JupyterStory = None  # type: ignore
 
 try:
+    import streamlit as st
     from .env.st.story import Story as StreamlitStory
+
+    if not st.scriptrunner.script_run_context.get_script_run_ctx():  # pragma: no cover
+        raise ImportError("StreamlitStory")
 except ImportError:  # pragma: no cover
     StreamlitStory = None  # type: ignore
 
