97197cc5c1c64b8128ac27305e59e04c78fe8293
David Vegh <david.andras.vegh+github@gmail.com>
2022-07-26 09:20:34 +0200

    env.py: improve docstring

--- src/ipyvizzustory/env/py/story.py
+++ src/ipyvizzustory/env/py/story.py
@@ -1,4 +1,4 @@
-"""A module for working with ipyvizzu-story presentations."""
+"""A module for working with presentation stories in Python environment."""
 
 from typing import Optional
 
@@ -11,9 +11,25 @@ class Story(StoryLib):
     """A class for representing a presentation story in Python environment."""
 
     def __init__(self, data: Data, style: Optional[Style] = None):
+        """
+        Presentation Story constructor.
+
+        Args:
+            data: Data set for the whole presentation story.
+                After initialization `data` can not be modified,
+                but it can be filtered.
+            style (optional): Style settings for the presentation story.
+                `style` can be changed at each presentation step.
+        """
+
         super().__init__(data=data, style=style)
 
     def play(self) -> str:
-        """A method for returning the html code."""
+        """
+        A method for returning the assembled html code in Python environment.
+
+        Returns:
+            The assembled html code as string.
+        """
 
         return self.to_html()
