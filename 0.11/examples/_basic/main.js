bae7fb9cb6ec8fc4a93af3eb676f9f2b39ca58b6
David Vegh <veghdev@gmail.com>
2024-07-10 10:26:54 +0000

    Deployed bd59855 to 0.11 with MkDocs 1.5.3 and mike 2.0.0

---
+++ 0.11/examples/_basic/main.js
@@ -0,0 +1,50 @@
+// Import VizzuPlayer
+// eslint-disable-next-line no-unused-vars
+import VizzuPlayer from 'https://cdn.jsdelivr.net/npm/vizzu-story@0.7/dist/vizzu-story.min.js'
+
+// Get the created element
+const vp = document.querySelector('vizzu-player')
+
+// Create data object
+const data = {
+	series: [
+		{
+			name: 'Foo',
+			values: ['Alice', 'Bob', 'Ted']
+		},
+		{
+			name: 'Bar',
+			values: [15, 32, 12]
+		},
+		{
+			name: 'Baz',
+			values: [5, 3, 2]
+		}
+	]
+}
+
+// Each slide here is a page in the final interactive story
+const slides = [
+	{
+		config: {
+			x: 'Foo',
+			y: 'Bar'
+		}
+	},
+	{
+		config: {
+			color: 'Foo',
+			x: 'Baz',
+			geometry: 'circle'
+		}
+	}
+]
+
+// Create story configuration object, add data and slides to it
+const story = {
+	data,
+	slides
+}
+
+// Set up the created element with the configuration object
+vp.slides = story
