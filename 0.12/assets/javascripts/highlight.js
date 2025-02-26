0c5f97a19b719aa1c631539c7bb773bc537d7e59
David Vegh <veghdev@gmail.com>
2025-02-26 16:51:32 +0000

    Deployed 199d480 to 0.12 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 0.12/assets/javascripts/highlight.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	document.querySelectorAll('pre code').forEach((el) => {
+		if (window.location.href.includes('LICENSE')) {
+			return
+		}
+
+		if (window.location.href.includes('/reference/ipyvizzustory/')) {
+			el.classList.add('language-python')
+		}
+
+		hljs.highlightElement(el) // eslint-disable-line no-undef
+		if (
+			window.location.href.includes('/examples/') ||
+			window.location.href.includes('/reference/') ||
+			window.location.href.includes('/environments/')
+		) {
+			hljs.lineNumbersBlock(el) // eslint-disable-line no-undef
+		}
+	})
+})
---
+++ 0.12/examples/titanic/main.js
@@ -0,0 +1,145 @@
+// Import VizzuPlayer
+// eslint-disable-next-line no-unused-vars
+import VizzuPlayer from 'https://cdn.jsdelivr.net/npm/vizzu-story@0.8/dist/vizzu-story.min.js'
+import Csv2Js from '../../assets/javascripts/csv2js.js'
+
+// Get the created element
+const vp = document.querySelector('vizzu-player')
+const vizzuLoaded = import(vp.vizzuUrl)
+
+// Create data object
+const dataLoaded = Csv2Js.csv('./titanic_js.csv', { dimensions: ['Pclass'] })
+
+Promise.all([dataLoaded, vizzuLoaded]).then((results) => {
+	const data = results[0]
+	const Vizzu = results[1].default
+
+	// Set the style of the charts in the story
+	const style = {
+		plot: {
+			yAxis: {
+				label: { fontSize: '1em', paddingRight: '1.2em' },
+				title: { color: '#ffffff00' }
+			},
+			xAxis: {
+				label: {
+					angle: '2.5',
+					fontSize: '1.1em',
+					paddingRight: '0em',
+					paddingTop: '1em'
+				},
+				title: { fontSize: '1em', paddingTop: '2.5em' }
+			}
+		},
+		logo: { width: '5em' }
+	}
+
+	// Each slide here is a page in the final interactive story
+	const slides = [
+		[
+			{
+				config: Vizzu.presets.bar({
+					x: 'Count',
+					title: 'Passengers of the Titanic'
+				})
+			}
+		],
+		[
+			{ config: Vizzu.presets.stackedBar({ x: 'Count', stackedBy: 'Sex' }) },
+			{
+				config: Vizzu.presets.groupedBar({
+					x: 'Count',
+					y: 'Sex',
+					legend: 'color',
+					title: 'Rougly one-third of the passengers were ladies'
+				})
+			}
+		],
+		[
+			{
+				config: {
+					x: ['Count', 'Survived'],
+					y: 'Sex',
+					color: 'Sex',
+					lightness: 'Survived',
+					label: ['Survived', 'Count']
+				}
+			},
+			{
+				config: {
+					align: 'stretch',
+					title: 'Much more women survived than men'
+				}
+			}
+		],
+		[
+			{
+				config: {
+					x: 'Count',
+					align: 'none',
+					label: null,
+					lightness: null,
+					title: "Let's add the age of the passengers to the mix"
+				}
+			},
+			{ config: { y: ['Count', 'Sex'], x: 'Age_group', label: 'Count' } }
+		],
+		[
+			{
+				config: {
+					label: null,
+					title: "Let's see how many people survived/died in these age groups"
+				}
+			},
+			{
+				config: {
+					y: ['Count', 'Sex', 'Survived'],
+					lightness: 'Survived',
+					legend: 'lightness'
+				}
+			},
+			{ config: { y: ['Count', 'Survived', 'Sex'] } }
+		],
+		[
+			{
+				config: {
+					align: 'stretch',
+					title: 'Survival rate varies a bit between age groups'
+				}
+			}
+		],
+		[
+			{
+				filter: (record) => record.Sex === 'male',
+				config: {
+					title: 'But again shows a very different picture for men...'
+				}
+			}
+		],
+		[
+			{ filter: null },
+			{
+				filter: (record) => record.Sex === 'female',
+				config: { title: '...and women' }
+			}
+		]
+	]
+
+	// Create story configuration object, add data and slides to it
+	const story = {
+		data,
+		style,
+		slides
+	}
+
+	// Set the size of the HTML element
+	vp.style.cssText = 'width: 100%; height: 400px;'
+
+	// Set up the created element with the configuration object
+	vp.slides = story
+	vp.initializing.then((chart) => {
+		// Switch on the tooltip that appears
+		// when the user hovers the mouse over a chart element
+		chart.feature('tooltip', true)
+	})
+})
